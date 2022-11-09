735a6ad04204eae19e39716f8cc976f04562de03
AroldoJerez <107219476+AroldoJerez@users.noreply.github.com>
2022-11-09 11:14:41 -0300

    aroldo

--- contacto/src/components/form.js
+++ contacto/src/components/form.js
@@ -4,15 +4,18 @@ import "../css/formulario.css";
 
 function AgregarContacto() {
   const url = "http://localhost:5000/api/agregarcontacto";
+  const [ms, setMs] = useState({
+    estado: "inactive",
+    Message: "",
+  });
   const [data, setData] = useState({
     Nombre: "",
     Numero: "",
   });
 
-  const Message = "test de enviado correctamente!";
-
   const submitForm = async (e) => {
     e.preventDefault();
+    console.log(e);
     await axios
       .post(url, {
         Nombre: data.Nombre,
@@ -20,10 +23,18 @@ function AgregarContacto() {
       })
       .then((res) => {
         if (res) {
+          setMs({
+            estado: "activems",
+            Message: `Contacto ${data.Nombre} agregado corretamente`,
+          });
           setTimeout(() => window.location.replace("/"), 7000);
         }
       })
       .catch((e) => {
+        setMs({
+          estado: "errorms",
+          Message: "Error inesperado",
+        });
         console.log(e);
       });
   };
@@ -33,35 +44,48 @@ function AgregarContacto() {
     newcontact[e.target.id] = e.target.value;
     setData(newcontact);
   };
-
-  return (
-    <div className="Formulario">
-      <div className="alert alert-success">{Message}</div>
-
-      <h1>Agregar Contacto</h1>
-      <form onSubmit={(e) => submitForm(e)}>
-        <input
-          onChange={(e) => handle(e)}
-          value={data.Nombre}
-          id="Nombre"
-          className="Nombre"
-          name="Nombre"
-          type="text"
-          placeholder="Ingresar Nombre"
-        ></input>
-        <input
-          onChange={(e) => handle(e)}
-          value={data.Numero}
-          id="Numero"
-          className="Numero"
-          name="Numero"
-          type="number"
-          placeholder="+54 999 9999999"
-        ></input>
-        <button className="btn btn-primary w-100 mb-2">Agregar Contacto</button>
-      </form>
-    </div>
-  );
+  if (ms.estado === "activems") {
+    return (
+      <div className="Formulario">
+        <div className="alert alert-success activems">{ms.Message}</div>
+      </div>
+    );
+  } else if (ms.estado === "errorms") {
+    return (
+      <div className="Formulario">
+        <div className="alert alert-success errorms">{ms.Message}</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Formulario">
+        <h1>Agregar Contacto</h1>
+        <form onSubmit={(e) => submitForm(e)}>
+          <input
+            onChange={(e) => handle(e)}
+            value={data.Nombre}
+            id="Nombre"
+            className="Nombre"
+            name="Nombre"
+            type="text"
+            placeholder="Ingresar Nombre"
+          ></input>
+          <input
+            onChange={(e) => handle(e)}
+            value={data.Numero}
+            id="Numero"
+            className="Numero"
+            name="Numero"
+            type="number"
+            placeholder="+54 999 9999999"
+          ></input>
+          <button className="btn btn-primary w-100 mb-2">
+            Agregar Contacto
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AgregarContacto;
